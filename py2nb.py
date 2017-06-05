fb18e722180c2019feb58764bbf997e1d2f881f4
prtx <prtx.pratikshrestha@gmail.com>
2017-06-05 16:00:10 +0545

    Update rules for markdown cells

--- py2nb.py
+++ py2nb.py
@@ -10,30 +10,41 @@ FILE = sys.argv[1]
 
 nb_cells = []
 flag_nl, buffer, pline = 0, list(), -1
+buffer_token = set()
+
 for token in tokenize.tokenize(open(FILE, 'rb').readline):
+    
     # print(token)
+
     if token.end == (0, 0): continue # auto encoding
     if token.start[0] == 1 and token.line.startswith('#!'): continue # auto encoding
     if token.type == 4: continue # line break
 
     if token.start[0] > pline: buffer.append(token.line)
+    buffer_token.add(token.type)
     pline = token.start[0]
 
-    if token.type == 57: # comment
-        if re.search('^# \*+ ', token.line):
-            buffer.pop()
-            block = "".join(buffer).strip()
-            if block: nb_cells.append(nbf4.new_code_cell(source=block))
-            heading = token.line.replace('*', '#')
-            nb_cells.append(nbf4.new_markdown_cell(source=heading.strip()))
-            buffer.clear()
-
+    # if token.type == 57: # comment
+    #     if re.search('^# \*+ ', token.line):
+    #         buffer.pop()
+    #         block = "".join(buffer).strip()
+    #         if block: nb_cells.append(nbf4.new_code_cell(source=block))
+    #         heading = token.line.replace('*', '#')
+    #         nb_cells.append(nbf4.new_markdown_cell(source=heading.strip()))
+    #         buffer.clear()
+    #
 
     if token.type == 58: # new line
         if flag_nl < 1: flag_nl += True; continue
+        comment_block = buffer_token.issubset({57,58})
         block = "".join(buffer).strip()
         buffer.clear()
-        if block: nb_cells.append(nbf4.new_code_cell(source=block))
+        buffer_token.clear()
+        if block:
+            if comment_block:
+                nb_cells.append(nbf4.new_markdown_cell(source=block.replace("#", "").replace("*", "#")))
+            else:
+                nb_cells.append(nbf4.new_code_cell(source=block))
         continue
 
     flag_nl = False
