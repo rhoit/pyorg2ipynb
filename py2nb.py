ffca7ad43d04cf66b8a72f7b1fb5abb8cc522fc7
prtx <prtx.pratikshrestha@gmail.com>
2017-06-06 23:01:16 +0545

    indentation cells not splitting smartly

--- py2nb.py
+++ py2nb.py
@@ -9,45 +9,41 @@ import re
 FILE = sys.argv[1]
 
 nb_cells = []
-flag_nl, buffer, pline = 0, list(), -1
+buffer = list()
 buffer_token = set()
+pline = -1
+flag_cl = False  # line comment flag
+flag_pcl = False # previous line comment flag
+flag_il = False  # line indent flag
 
 for token in tokenize.tokenize(open(FILE, 'rb').readline):
     
-    # print(token)
 
     if token.end == (0, 0): continue # auto encoding
     if token.start[0] == 1 and token.line.startswith('#!'): continue # auto encoding
-    if token.type == 4: continue # line break
+    if token.start[1] != 0 : continue 
+    if token.type in [4, 58]: continue # line break
 
-    if token.start[0] > pline: buffer.append(token.line)
-    buffer_token.add(token.type)
-    pline = token.start[0]
+    
+    flag_pcl = flag_cl
+    flag_cl = token.type == 57
+    flag_il = token.type in [5, 6]
 
-    # if token.type == 57: # comment
-    #     if re.search('^# \*+ ', token.line):
-    #         buffer.pop()
-    #         block = "".join(buffer).strip()
-    #         if block: nb_cells.append(nbf4.new_code_cell(source=block))
-    #         heading = token.line.replace('*', '#')
-    #         nb_cells.append(nbf4.new_markdown_cell(source=heading.strip()))
-    #         buffer.clear()
-    #
+    print(token)
 
-    if token.type == 58: # new line
-        if flag_nl < 1: flag_nl += True; continue
-        comment_block = buffer_token.issubset({57,58})
-        block = "".join(buffer).strip()
-        buffer.clear()
-        buffer_token.clear()
-        if block:
-            if comment_block:
-                nb_cells.append(nbf4.new_markdown_cell(source=block.replace("#", "").replace("*", "#")))
-            else:
-                nb_cells.append(nbf4.new_code_cell(source=block))
-        continue
+    if flag_pcl:
+        if not flag_cl:
+            block = "".join(buffer).strip()
+            buffer.clear()
+            nb_cells.append(nbf4.new_markdown_cell(source=block.replace("#", "").replace("*", "#")))
+    else:
+        if not flag_il:
+            block = "".join(buffer).strip()
+            buffer.clear()
+            nb_cells.append(nbf4.new_code_cell(source=block))
 
-    flag_nl = False
+    buffer.append(token.line)
+ 
 else:
     nb_cells.append(nbf4.new_code_cell(source="".join(buffer).strip()))
 
